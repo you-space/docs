ce2309b1a9fd9fe0c3e2e90e58ca19f2da82e7a0
Henrique <henryqueoli@hotmail.com>
2021-09-10 15:13:23 -0300

    feat: add project logo

--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,12 @@
 ---
 home: true
+heroImage: /logo-circle-200x200.png
+actions:
+    - text: Install with docker
+      link: /installation/docker.md
+      type: primary
+
+    - text: Install with zip file
+      link: /installation/using-build-zip.md
+      type: secondary
 ---
--- vuepress.config.ts
+++ vuepress.config.ts
@@ -1,29 +1,32 @@
-import { defineUserConfig } from 'vuepress'
-import type { DefaultThemeOptions } from 'vuepress'
+import { defineUserConfig } from "vuepress";
+import type { DefaultThemeOptions } from "vuepress";
 
-import { en, ptBR } from './configs'
+import { en, ptBR } from "./configs";
 
-const isProduction = process.env.NODE_ENV === "production"
+const isProduction = process.env.NODE_ENV === "production";
 
 export default defineUserConfig<DefaultThemeOptions>({
     base: isProduction ? "/docs/" : "/",
+    head: [["link", { rel: "icon", href: "/logo-circle-200x200.png" }]],
     // site-level locales config
     locales: {
-        '/': {
-            lang: 'en-US',
-            title: 'You space',
-            description: 'Content management system',
+        "/": {
+            lang: "en-US",
+            title: "You space",
+            description: "Content management system",
         },
-        '/pt-BR/': {
-                lang: 'pt-BR',
-                title: 'You space',
-                description: 'Sistema de administração de conteúdos',
-        },
+        // '/pt-BR/': {
+        //         lang: 'pt-BR',
+        //         title: 'You space',
+        //         description: 'Sistema de administração de conteúdos',
+        // },
     },
     themeConfig: {
+        editLink: true,
+        repo: "you-space/docs",
         locales: {
-            '/': en,
-            '/pt-BR/': ptBR,
-        }
-    }
-})
+            "/": en,
+            // '/pt-BR/': ptBR,
+        },
+    },
+});
